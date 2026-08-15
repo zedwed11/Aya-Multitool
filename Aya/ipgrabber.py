73695506f79cc2b2ac63005f41ac0ecdbbf3f91f
zed <asdvr1289@gmail.com>
2026-08-15 02:54:51 -0700

    Delete Aya directory to add new tools

--- Aya/ipgrabber.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-def run():
-    default_browser = webbrowser.get()
-    default_browser.open('https://grabify.link/')
-    default_browser.open('https://discord.gg/cCKctNPvE6')
- 
